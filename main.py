6ed7ccfb79aaf96f41a33faa91e0054c5b667710
wiedzmok <pawelbarnas8@gmail.com>
2025-03-13 23:12:37 +0100

    add

--- main.py
+++
@@ -1,9 +0,0 @@
-import importlib
-import zad_1abc
-import numpy as np
-import matplotlib.pyplot as plt
-importlib.reload(zad_1abc)
-zad_1abc.task_A()
-zad_1abc.task_B()
-zad_1abc.task_C()
-# Compare this snippet from zad_1abc.py:
